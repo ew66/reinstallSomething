ff6a1825b9405e16f42ae54bf7cbb7f2e353e85e
edgarcywang <EdgarCYWang@fih-foxconn.com>
2015-07-27 14:10:51 +0800

    Support Windows and use -i as default, it means you just key reinstallSomething.py something to push something into your device

--- reinstallSomething.py
+++ reinstallSomething.py
@@ -11,6 +11,8 @@ import time
 import datetime
 from subprocess import call, Popen, PIPE
 import argparse
+import platform
+import glob
 
 ######################
 ##     function     ##
@@ -67,9 +69,9 @@ def pullSomething(action, path):
 #	-i: interactive commit message
 #	-c: comment in code
 parser = argparse.ArgumentParser(description='Reinstall Something in your $OUT/system')
-group = parser.add_mutually_exclusive_group(required=True)
+group = parser.add_mutually_exclusive_group()
 parser.add_argument('something_name_list', default='', nargs='*', help='ex: Camera2.apk, library_name.so')
-group.add_argument('-i', help='install something', action='store_const', const=1)
+group.add_argument('-i', help='install something(default)', action='store_const', const=1)
 group.add_argument('-r', help='remove something', action='store_const', const=2)
 group.add_argument('-p', help='pull something', action='store_const', const=3)
 group.add_argument('-I', help='overly path in env $OUT_OVERLAY', action='store_const', const=4)
@@ -78,40 +80,82 @@ args = parser.parse_args()
 use_overlay = False;
 if args.I != None:
     use_overlay = True;
+elif args.i == None and args.p == None and args.r == None:
+    args.I = True; # Set default is -i
     
 something_name_list = args.something_name_list
+
+os_windows_separator = '\\'
+os_unix_separator = '/'
+os_dir_separator = os_unix_separator
+is_os_windows = False
+os_uname = platform.uname()[0]
+if os_uname == "Windows":
+    is_os_windows = True
 
 #print args.something_name_list
 #exit();
 serial = getDevices()
-# get $OUT path
-env_out_path = os.environ.copy()["OUT"]
-if use_overlay == True:
-    try:
-        overlay_path = os.environ.copy()["OUT_OVERLAY"]
-        if overlay_path:
-            env_out_path = overlay_path
-    except:
-        pass
+
+if is_os_windows:
+    os_dir_separator = os_windows_separator
+    out_path = os.path.expanduser('~') + os_dir_separator + "OUT_PATH"
+    if not os.path.isfile(out_path):
+        print "Missing codebase outpath"
+        exit()
+    env_out_path = open(out_path, 'r').read().strip('\n')
+else:
+    # get $OUT path
+    env_out_path = os.environ.copy()["OUT"]
+    if use_overlay == True:
+        try:
+            overlay_path = os.environ.copy()["OUT_OVERLAY"]
+            if overlay_path:
+                env_out_path = overlay_path
+        except:
+            pass
+
 
 # search path=$OUT/system
-something_root_path = env_out_path + '/system'
+something_root_path = env_out_path + os_dir_separator + 'system'
 
 for something_name in something_name_list:
+    something_path = ''
+    something_path_list = []
     # find something path 
-    something_path, err = Popen(["find", something_root_path, "-name", something_name], stdout=PIPE).communicate()
-    if something_path == '':
-    	print "Error: Can not find " + something_name + " in " + something_root_path
-    	break
-    # something_path=$OUT/system/something_dest_path
-    something_path = something_path.strip()
-    something_path_list = something_path.split('\n')
-    push_operation = False
+    if is_os_windows:
+        search_list = []
+        result = []
+        search_list.append(something_root_path)
+        for dpath, dnames, fnames in os.walk(something_root_path):
+            for i, dname in enumerate([os.path.join(dpath, dname) for dname in dnames]):
+                search_list.append(dname)
+
+        for path in search_list:
+            result.extend(glob.glob(path + os_dir_separator + something_name))
+        if result == []:
+            print "Error: Can not find " + something_name + " in " + something_root_path
+            break
+        something_path_list = result
+    else:
+        something_path, err = Popen(["find", something_root_path, "-name", something_name], stdout=PIPE).communicate()
+        if something_path == '':
+            print "Error: Can not find " + something_name + " in " + something_root_path
+            break
+
+        # something_path=$OUT/system/something_dest_path
+        something_path = something_path.strip()
+        something_path_list = something_path.split('\n')
     
+    push_operation = False
     for something in something_path_list:
        push_operation = True
        something = something.strip()
        something_dest_path = something.replace(env_out_path,'')
+       if is_os_windows:
+           print something_dest_path
+           something_dest_path = something_dest_path.replace(os_windows_separator, os_unix_separator)
+           print something_dest_path
        
        # push something
        if args.i != None or args.I != None:
